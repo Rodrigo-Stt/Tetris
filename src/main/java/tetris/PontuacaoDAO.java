a2b91f46641d62c0e5086f71023ecd8d8c6d85f6
Rodrigo-Stt <rodrigosantosrocha263@gmail.com>
2026-05-13 22:03:24 -0300

    feat: implementar PontuacaoDAO com conexão ao MongoDB
    - Adicionada conexão ao banco de dados MongoDB
    - Implementado padrão Singleton para gerenciar a instância do DAO
    - Implementado método salvarPontuacao()
    - Implementado método obterTopPontuacaos() com ordenação por pontos
      e limite configurável

--- src/main/java/tetris/PontuacaoDAO.java
+++ src/main/java/tetris/PontuacaoDAO.java
@@ -1,8 +1,60 @@
 package tetris;
 
+import com.mongodb.client.*;
+import org.bson.Document;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PontuacaoDAO {
 
-    public static void main(String[] args) {
+    private static PontuacaoDAO instancia; 
+    private final MongoCollection<Document> col; 
+    private PontuacaoDAO(){
+
+        MongoClient cli = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase db = cli.getDatabase("tetris");
+        col = db.getCollection("pontuacoes");
+
+    }
+    public static PontuacaoDAO getInstancia() {
+        if (instancia == null) 
+            
+            instancia = new PontuacaoDAO();
+        
+        return instancia;
         
     }
+
+    public void salvarPontuacao(ModeloPontuacao mp){
+        Document d = new Document("nome", mp.getNome())
+                            .append("pontos", mp.getPontos())
+                            .append("data", mp.getData());
+
+        col.insertOne(d);
+    }
+
+    public List<ModeloPontuacao> obterTopPontuacaos(int n){
+
+        List<ModeloPontuacao> lista = new ArrayList<>();
+
+        col.find()
+
+        .sort(new Document("pontos", -1).append("data", 1))
+
+        .limit(n)
+
+        .forEach(d ->
+            lista.add(new ModeloPontuacao(
+                d.getString("nome"),
+                d.getInteger("pontos"),
+                d.getDate("data")      ////////////////// ATENÇÃO ALTERAR PARA getString ou getDate
+            ))
+
+        );
+
+    return lista;
+
+    }
+
 }
+
